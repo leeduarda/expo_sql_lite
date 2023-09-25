0abf21248c8f68b39e731cca242fa0a6285db711
Aula AndroidStudio <aula.android@fatectq.local>
2023-09-25 11:23:08 -0300

    Projeto banco de dados

---
+++ mobile/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Projeto com banco de dados</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ mobile/src/database/db-init.tsx
@@ -0,0 +1,58 @@
+import { DatabaseConnection } from './db-connection'
+
+var db = null
+export default class DatabaseInit {
+
+    constructor() {
+        db = DatabaseConnection.getConnection()
+        db.exec([{ sql: 'PRAGMA foreign_keys = ON;', args: [] }], false, () =>
+        console.log('Foreign keys turned on')
+    );
+        this.InitDb()
+    }
+    private InitDb() {
+        var sql = [
+            `DROP TABLE IF EXISTS animal;`,
+            `DROP TABLE IF EXISTS gato;`,
+            `DROP TABLE IF EXISTS cachorro;`,
+
+            `create table if not exists animal (
+            id integer primary key autoincrement,
+            nome text,
+         
+            );`,
+            `create table if not exists gato (
+            id integer primary key autoincrement,
+            nome text,
+            animal_id int,
+            foreign key (animal_id) references animal (id)
+            );`,
+             `create table if not exists cachorro (
+            id integer primary key autoincrement,
+            nome text,
+            animal_id int,
+            foreign key (animal_id) references animal (id)
+            );`,
+            
+            `insert into gato(nome) values('preto');`,
+            `insert into gato(nome) values('cinza');`,
+            `insert into cachorro (nome) values('vira lata');`,
+            `insert into cachorro (nome) values('salsicha');`
+        ];
+
+        db.transaction(
+            tx => {
+                for (var i = 0; i < sql.length; i++) {
+                    console.log("execute sql : " + sql[i]);
+                    tx.executeSql(sql[i]);
+                }
+            }, (error) => {
+                console.log("error call back : " + JSON.stringify(error));
+                console.log(error);
+            }, () => {
+                console.log("transaction complete call back ");
+            }
+        );
+    }
+
+}
